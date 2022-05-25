84fca0ef88c8d1a4d9147992ed6ebb6574b75c49
jordi <jordi.lanau@yahoo.com>
2022-05-25 11:22:41 +0100

    Establish DB connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import path from 'path';
+import db from './config/connection';
 
 const app = express();
 
@@ -12,8 +13,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/build')));
 }
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_, res: Response) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('*', (_, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
+
+db.once('open', () => {
+  console.log('Connection to database established');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
