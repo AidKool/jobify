36476ba6f521f3f9a53bc686c6f2f10542515aa2
Jordi <jordi.lanau@yahoo.com>
2022-06-13 01:05:04 +0100

    Downgrade React version to 17.0.2
    Downgrade React version to solve compatibility issues with the recharts library

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { AppProvider } from './context/AppContext';
+import ReactDOM from 'react-dom';
+
 import App from './App';
 
 import '@fontsource/roboto/300.css';
@@ -8,9 +8,9 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
